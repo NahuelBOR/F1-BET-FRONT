f90bfb2542c936b9e39555ae2f80277d1dfa7f22
NahuelBOR <107086688+NahuelBOR@users.noreply.github.com>
2025-06-20 14:35:20 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // frontend/src/config.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://f1-bet-back.onrender.com/api';
 
 export default API_BASE_URL;
